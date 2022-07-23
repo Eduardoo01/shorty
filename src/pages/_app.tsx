5079bdb94cfd741373f370803a289cbb9e1393d7
Eduardo Donnarumma <edonnarumma@wheretech.it>
2022-07-23 17:11:37 +0200

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,25 +2,44 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { withTRPC } from "@trpc/next";
 import { AppRouter } from "./api/trpc/[trpc]";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Script
+				strategy="lazyOnload"
+				src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+			/>
+			<Script strategy="lazyOnload">
+				{`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+			</Script>
+			<Component {...pageProps} />;
+		</>
+	);
 }
 
 function getBaseUrl() {
-  if (process.browser) return ""; // Browser should use current path
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+	if (process.browser) return ""; // Browser should use current path
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+	return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
 }
 
 export default withTRPC<AppRouter>({
-  config() {
-    const url = `${getBaseUrl()}/api/trpc`;
+	config() {
+		const url = `${getBaseUrl()}/api/trpc`;
 
-    return {
-      url,
-    };
-  },
-  ssr: false,
+		return {
+			url,
+		};
+	},
+	ssr: false,
 })(MyApp);
